8be36b3ea63f92631e50d87e155f29171c11afc4
Benjamin Reed <ranger@opennms.org>
2016-05-06 12:06:57 -0400

    chore(): rename REPODIR to WORKDIR

--- clean-repo.sh
+++ clean-repo.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-REPODIR="$1"; shift
+WORKDIR="$1"; shift
 
-if [ -z "$REPODIR" ] || [ ! -d "$REPODIR" ]; then
-	echo "usage: $0 <repodir>" >&2
+if [ -z "$WORKDIR" ] || [ ! -d "$WORKDIR" ]; then
+	echo "usage: $0 <working-directory>" >&2
 	echo "" >&2
 	exit 1
 fi
@@ -12,4 +12,4 @@ if [ -d "$HOME/.m2" ]; then
 	rm -rf "$HOME"/.m2/repository*/org/opennms
 fi
 
-find "$REPODIR" -type d -name target -print0 | xargs -0 rm -rf
+find "$WORKDIR" -type d -name target -print0 | xargs -0 rm -rf
