4590f255a3c4a17df96dcf50c107be4b26bb03a3
Benjamin Reed <ranger@opennms.org>
2016-05-10 13:06:19 -0400

    feat(environment): set path and a better environment variable for the build number

--- environment.sh
+++ environment.sh
@@ -9,3 +9,7 @@ export JICMP_USE_SOCK_DGRAM="1"
 export COMPILE_OPTIONS=("-DupdatePolicy=never" "-Dmaxmemory=2g" "-Dorg.ops4j.pax.url.mvn.localRepository=$M2_LOCAL_REPOSITORY")
 export SKIP_TESTS=("-Dmaven.test.skip.exec=true" "-DskipITs=true")
 export ENABLE_TESTS=("-Dmaven.test.skip.exec=false" "-DskipITs=false")
+export PATH="/usr/local/bin:$PATH"
+
+# shellcheck disable=SC2154
+export BAMBOO_BUILD_NUMBER="${bamboo_buildNumber}"
