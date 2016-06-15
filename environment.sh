467c661779c40ad82faa6a9969078b1791ecc926
Benjamin Reed <ranger@opennms.org>
2016-06-15 16:24:38 -0400

    reinstate "allow parallel compiles"
    This reverts commit 79f8e6fe43a3b3b50ef346cae8ffd6d637dbe0f0.

--- environment.sh
+++ environment.sh
@@ -6,7 +6,7 @@ fi
 
 export M2_LOCAL_REPOSITORY="$HOME/.m2/repository-${bamboo_buildKey}"
 export JICMP_USE_SOCK_DGRAM="1"
-export COMPILE_OPTIONS=("-DupdatePolicy=never" "-Dmaxmemory=2g" "-Dorg.ops4j.pax.url.mvn.localRepository=$M2_LOCAL_REPOSITORY")
+export COMPILE_OPTIONS=("-DupdatePolicy=never" "-Dmaxmemory=2g" "-Dorg.ops4j.pax.url.mvn.localRepository=$M2_LOCAL_REPOSITORY" "-Daether.connector.basic.threads=1" "-Daether.connector.resumeDownloads=false")
 export SKIP_TESTS=("-Dmaven.test.skip.exec=true" "-DskipITs=true")
 export ENABLE_TESTS=("-Dmaven.test.skip.exec=false" "-DskipITs=false")
 export PATH="/usr/local/bin:$PATH"
