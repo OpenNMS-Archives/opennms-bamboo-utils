938aeaa164ac3d8346dd6fef89c7cdabacd7b1f1
Benjamin Reed <ranger@opennms.org>
2017-11-29 17:39:11 -0500

    separate out method to get git branch name

--- lib/bamboo.sh
+++ lib/bamboo.sh
@@ -20,7 +20,7 @@ get_branch_name() {
 	if [ -z "$_branch_name" ] || [ "$(echo "$_branch_name" | grep -c '\$')" -gt 0 ]; then
 		# branch did not get substituted, use git instead
 		echo "WARNING: \$bamboo_OPENNMS_BRANCH_NAME and \$bamboo_planRepository_branchName are not set, attempting to determine branch with \`git symbolic-ref HEAD\`." >&2
-		_branch_name="$( (cd "${_workdir}" || exit 1; git symbolic-ref HEAD) | sed -e 's,^refs/heads/,,')"
+		_branch_name="$(get_git_branch_name "${_workdir}")"
 	fi
 
 	echo "${_branch_name}"
--- lib/git.sh
+++ lib/git.sh
@@ -20,4 +20,16 @@ get_git_date() {
 	git log --pretty='format:%cd' --date='short' -1 | sed -e 's,-,,g'
 }
 
+get_git_branch_name() {
+	local _workdir
+	local _branch_name
+
+	_workdir="$1"; shift
+
+	set +u
+	_branch_name="$( (cd "${_workdir}" || exit 1; git symbolic-ref HEAD) | sed -e 's,^refs/heads/,,')"
+	echo "${_branch_name}"
+	set -u
+}
+
 set +euo pipefail
