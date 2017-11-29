ad9018473506f33bdc3a7cc7e3523c8610eb775f
Benjamin Reed <ranger@opennms.org>
2017-11-29 17:30:07 -0500

    use unique file when excluding

--- lib/docker.sh
+++ lib/docker.sh
@@ -10,18 +10,19 @@ reset_docker() {
 	(docker kill $(docker ps --no-trunc -a -q)) 2>/dev/null || :
 
 	if [ -e /var/run/docker.sock ]; then
-		cat <<END >/tmp/docker-gc-exclude.txt
+		cat <<END >/tmp/docker-gc-exclude.$$
 opennms/*
 stests/*
 END
 		# garbage-collect old docker containers and images
-		(docker run -v /tmp/docker-gc-exclude.txt:/tmp/docker-gc-exclude.txt -v /var/run/docker.sock:/var/run/docker.sock spotify/docker-gc env MINIMUM_IMAGES_TO_SAVE=1 EXCLUDE_FROM_GC=/tmp/docker-gc-exclude.txt /docker-gc) 2>/dev/null || :
+		(docker run -v /tmp/docker-gc-exclude.$$:/tmp/docker-gc-exclude.txt -v /var/run/docker.sock:/var/run/docker.sock spotify/docker-gc env MINIMUM_IMAGES_TO_SAVE=1 EXCLUDE_FROM_GC=/tmp/docker-gc-exclude.txt /docker-gc) 2>/dev/null || :
 	else
 		docker system prune --all --volumes --force 2>/dev/null || :
 	fi
 
 	# remove any dangling mounted volumes
 	docker system prune --volumes --force 2>/dev/null || :
+	rm -f /tmp/docker-gc-exclude.$$ || :
 	set -eo pipefail
 }
 
