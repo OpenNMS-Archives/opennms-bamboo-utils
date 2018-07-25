5ea51ba3a7dcc55cf54801ad1e00bb4d979c976a
Benjamin Reed <ranger@opennms.org>
2018-07-25 14:26:10 -0400

    put maven option (batch mode) after defines

--- environment.sh
+++ environment.sh
@@ -6,7 +6,7 @@ fi
 
 export M2_LOCAL_REPOSITORY="$HOME/.m2/repository-${bamboo_buildKey}"
 export JICMP_USE_SOCK_DGRAM="1"
-export COMPILE_OPTIONS=("-DupdatePolicy=never" "-Dmaxmemory=2g" "-Dorg.ops4j.pax.url.mvn.localRepository=$M2_LOCAL_REPOSITORY" "-Daether.connector.basic.threads=1" "-Daether.connector.resumeDownloads=false" "--batch-mode" "-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn")
+export COMPILE_OPTIONS=("-DupdatePolicy=never" "-Dmaxmemory=2g" "-Dorg.ops4j.pax.url.mvn.localRepository=$M2_LOCAL_REPOSITORY" "-Daether.connector.basic.threads=1" "-Daether.connector.resumeDownloads=false" "-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn" "--batch-mode")
 export SKIP_TESTS=("-Dmaven.test.skip.exec=true" "-DskipITs=true")
 export ENABLE_TESTS=("-Dmaven.test.skip.exec=false" "-DskipITs=false")
 export PATH="/usr/local/bin:$PATH"
