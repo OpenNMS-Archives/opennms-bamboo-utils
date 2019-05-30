bc844d61d85f2ac18ac0220172ebaf9dd9111c0d
Benjamin Reed <ranger@opennms.org>
2019-05-30 12:32:01 -0400

    don't output every file when tar/untar

--- backup-build.sh
+++ backup-build.sh
@@ -20,13 +20,13 @@ mkdir -p "$OUTPUTDIR"
 
 pushd "$WORKDIR" || exit 1
 	echo "* packing target directories to $OUTPUTDIR/target.tar.gz"
-	find . -type d -name target | tar -T - -cvzf "$OUTPUTDIR/target.tar.gz"
+	find . -type d -name target | tar -T - -czf "$OUTPUTDIR/target.tar.gz"
 popd || exit 1
 
 if [ -d "$REPODIR" ]; then
 	pushd "$REPODIR" || exit 1
 		echo "* packing $REPODIR to $OUTPUTDIR/repo.tar.gz"
-		tar -cvzf "$OUTPUTDIR/repo.tar.gz" .
+		tar -czf "$OUTPUTDIR/repo.tar.gz" .
 	popd || exit 1
 else
 	echo "repository $REPODIR does not exist"
