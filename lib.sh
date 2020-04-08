40508f52a1ae3414fc22f9f3deaecb10d3382938
Benjamin Reed <ranger@opennms.org>
2020-04-08 17:05:32 -0400

    this is a lot simpler

--- lib.sh
+++ lib.sh
@@ -168,25 +168,13 @@ reset_postgresql() {
 }
 
 reset_docker() {
-	echo "- killing and removing existing Docker containers..."
+	echo "- killing and removing old Docker containers..."
 	set +eo pipefail
 	# shellcheck disable=SC2046
 	# stop all running docker containers
 	(docker kill $(docker ps --no-trunc -a -q)) 2>/dev/null || :
-
-	if [ -e /var/run/docker.sock ]; then
-		cat <<END >/tmp/docker-gc-exclude.txt
-opennms/*
-stests/*
-END
-		# garbage-collect old docker containers and images
-		(docker run -v /tmp/docker-gc-exclude.txt:/tmp/docker-gc-exclude.txt -v /var/run/docker.sock:/var/run/docker.sock spotify/docker-gc env MINIMUM_IMAGES_TO_SAVE=1 EXCLUDE_FROM_GC=/tmp/docker-gc-exclude.txt /docker-gc) 2>/dev/null || :
-		docker system prune --volumes --force 2>/dev/null || :
-	else
-		docker system prune --all --volumes --force 2>/dev/null || :
-	fi
-
-	# remove any dangling mounted volumes
+	docker system prune --all --force --filter "until=24h" 2>/dev/null || :
+	docker system prune --force 2>/dev/null || :
 	docker system prune --volumes --force 2>/dev/null || :
 	set -eo pipefail
 }
